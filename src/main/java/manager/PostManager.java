1e92ca61de7b4afeafe938c3fcaf2705e552c036
Dmitriy Migorenko <migorenko.dmitry@yandex.ru>
2020-10-05 18:17:31 +0300

    PostManager added

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,14 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, int ownerOnly, int count, int offset) {
+        return posts;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+}
